6e1c2bd6c2a21fa346c6ca436db7a3dd94780cd7
teotoivo <teo.maximilien@gmail.com>
2025-04-23 16:01:28 +0300

    addded stepper_motor functions

---
+++ include/stepper_motor.h
@@ -0,0 +1,27 @@
+#ifndef STEPPER_MOTOR_H
+#define STEPPER_MOTOR_H
+
+#include <stdint.h>
+
+#include "globals.h"
+
+#define CALIBRATION_RUNS 1
+
+/**
+ * initializes everything needed for the stepper motor
+ * main still needs to set interrupt callback
+ */
+void init_stepper_motor(ProgramState *program_state);
+
+/**
+ * runs stepper motot for steps_to_run steps
+ * mark_in_eeprom is mainly for calibration since it handles marking it self
+ */
+void run_stepper_steps(ProgramState *program_state, int32_t steps_to_run,
+					   bool mark_in_eeprom);
+
+void opto_fork_interrupt_callback(uint32_t event_mask);
+
+void calibarate_stepper_motor(ProgramState *program_state);
+
+#endif	// !STEPPER_MOTOR_H
